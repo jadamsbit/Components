b203bc76e79929a58b2686d8c48734998b24845a
Garland Couch <couch.garland@bitsourceky.com>
2015-05-15 13:56:45 -0400

    Merged files from story #'s 8988, 9242, 9740, 9616

---
+++ js/addComponent.js
@@ -0,0 +1,183 @@
+/* 
+
+define the model
+*/
+
+
+
+
+
+// console.log(model);
+
+$( document ).ready(function() {
+	  // Handler for .ready() called.	
+initalize();
+
+	  /*===== this will hide the fields for the add features area. =====*/
+$('#showHide').hide();
+
+
+
+/*
+the model will contain
+all values to be presented and updated
+
+the model will be a json object
+
+define the controller
+the controller will contain two methods
+method 1 update output
+method 2 run calculations
+
+define the view
+the html
+
+update the view
+*/
+
+
+});
+
+
+function calculate(input){
+	//get the model
+	//loop thru each item in the model
+	for(var m=0; m < model.data.machines[model.selectedMachine].components.length; m++){ // loop thru all items
+		var oldValue = Number(model.data.machines[model.selectedMachine].components[m].hours); // at item 0, hours = 2,318 String
+		// console.log("old: " + oldValue);
+		// console.log("input: " + input);
+		var whatEver = input + oldValue; // take input, 5 (integer), add to 2,318 String???
+		// console.log("was: " + model.components[m].hours);
+		// console.log("new: " + whatEver);
+		model.data.machines[model.selectedMachine].components[m].hours = whatEver.toString(); // set hours to whatever value
+		// console.log("now: " + model.components[m].hours);
+	}
+	//get the input from the submit button
+
+	//apply the input to each hours value
+}
+
+
+function updateOutput(){
+	//get the model
+    // console.log(model);
+	//loop thru each item in the model
+	for(var i=0; i < model.data.machines[model.selectedMachine].components.length; i++){
+		//update the corresponding id in html
+		var currentComponent = model.data.machines[model.selectedMachine].components[i];
+		$("#" + model.data.machines[model.selectedMachine].components[i].id + "_hours p").html(currentComponent['hours']);
+		//console.log("#" + model.components[i].component + "_hours p")
+	}
+}
+
+$( "#submit" ).click(function(evt) {
+	//get the input value
+	var newValue = Number($("#input").val());
+	//console.log(newValue);
+	//run calculate
+	calculate(newValue);
+	//run update output
+	updateOutput();
+	checkLifeCycle();
+});
+
+/*===== this is the function to hide the add features button =====*/
+$( "#addComp" ).click(function(evt) {
+	$("#addComp").hide();
+	$("#showHide").show();
+	});
+
+
+	/*===== this is the function that will take in the user info and hide the fields and buuton and show the add button. =====*/
+$( "#insertComp" ).click(function(evt) {
+	$("#addComp").show();
+	$("#showHide").hide();
+	generateOutput()
+	});
+
+	// Input of the new data into the JSON object.
+function generateOutput(){
+	var newComp = $("#newComp").val();
+	var newHours = Number($("#newRoof").val());
+	var newProj = Number($("#newProj").val());
+	var newId = model.data.machines[model.selectedMachine].components.length + 1;
+	model.data.machines[model.selectedMachine].components.push({"id":newId, "component":newComp, "hours":newHours, "projection":newProj});
+	//loop through the json object
+	for(var i=0; i < model.data.machines[model.selectedMachine].components.length; i++){
+	//output a line item for each json object found
+	var id = model.data.machines[model.selectedMachine].components[i].id
+	var name = model.data.machines[model.selectedMachine].components[i].component
+	var hours = model.data.machines[model.selectedMachine].components[i].hours
+	var projection = model.data.machines[model.selectedMachine].components[i].projection
+	}
+	generateLineItem(id,name,hours,projection)
+}
+ 
+	// Inital load of all information in the JSON to the HTML.
+function initalize(){
+		//loop through the json object
+	 for(var i=0; i < model.data.machines[model.selectedMachine].components.length; i++){
+		//output a line item for each json object found
+	var id = model.data.machines[model.selectedMachine].components[i].id
+	var name = model.data.machines[model.selectedMachine].components[i].component
+	var hours = model.data.machines[model.selectedMachine].components[i].hours
+	var projection = model.data.machines[model.selectedMachine].components[i].projection
+	generateLineItem(id,name,hours,projection)
+	}
+}
+
+	// Generates the information and the new containers.
+function generateLineItem (id, name, hours, projection){
+		//take perameters and create HTML for each line item
+	$("#component").append("<div class='comp' id='" + id + "_comp'><p>" + name + "</p> </div>");
+	$("#hours").append("<div class='hours' id='" + id + "_hours'><p>" + hours + "</p> </div>");
+	$("#projection").append("<div class='proj' id='" + id + "_proj'><p>" + projection + "</p> </div>");
+	$("#part-item-input-").append("<div class='update' id='" + id + "_input'><input type='number' id='part-item-input-"+ id +"' value='0' min='0' max='24' step='.25'></input></div>");
+	$("#adjust1").append("<div class='adjust' id='" + id + "_adjust'><button type='button' data-button='"+ id +"' class='btn btn-info updateRecordButton'>Adjust</button></div>");
+
+	$( "#" + id + "_adjust button" ).click(function(evt) {
+		var id = $(this).data('button'); 
+		var newValue = Number($("#part-item-input-" + id).val());
+		updateRecord(newValue, id);
+		updateOutput();
+		checkLifeCycle();
+		
+	});	
+}
+
+
+	// Show/Hide for the Exit button
+$("#out").click(function(evt) {
+	$("#showHide").hide();
+	$("#addComp").show();
+});
+
+
+function updateRecord(newValue, componentId) {
+	//loop through the model's
+	for(var m=0; m < model.data.machines[model.selectedMachine].components.length; m++){ // loop thru all items
+		if (model.data.machines[model.selectedMachine].components[m].id === componentId){
+		var oldValue = Number(model.data.machines[model.selectedMachine].components[m].hours);
+		var whatEver = newValue + oldValue;
+		model.data.machines[model.selectedMachine].components[m].hours = whatEver.toString();
+		} 
+	}
+}
+
+function checkLifeCycle(){
+ 	for(var m=0; m < model.data.machines[model.selectedMachine].components.length; m++){ // loop thru all items
+		if(parseInt(model.data.machines[model.selectedMachine].components[m].hours) > parseInt(model.data.machines[model.selectedMachine].components[m].projection)){
+			alert("Maintenance Due Now On " + model.data.machines[model.selectedMachine].components[m].component.toUpperCase());
+		}
+		else if(parseInt(model.data.machines[model.selectedMachine].components[m].hours) > parseInt(model.data.machines[model.selectedMachine].components[m].projection) * 0.90){
+			alert("Maintenance Due Soon On " + model.data.machines[model.selectedMachine].components[m].component.toUpperCase());
+			console.log (model.data.machines[model.selectedMachine].components[m].component)
+		}
+		else
+		{
+		}
+ 	}
+}
+
+
+
---
+++ js/ajax.js
@@ -0,0 +1,82 @@
+
+
+model.scriptURL = "http://localhost:8888/components.php";
+
+model.updateModel = function (data){
+	model.data  = JSON.parse(data);
+	model.data = JSON.parse(model.data);
+	console.log(model.data);
+}
+
+model.getData = function (data){
+	var data = {
+	      "action": "load"
+	  };
+	data = $(this).serialize() + "&" + $.param(data);
+	$.ajax( {
+		method: "POST",
+		url: model.scriptURL,
+		dataType:"json",
+		data:data,
+		success: function (data){
+			model.updateModel(data["json"]);
+		}
+	});
+}
+
+model.saveData = function (data){
+	console.log(JSON.stringify(model.data));
+	var data = {
+	      "action": "save",
+	      "data" : JSON.stringify(model.data)
+	  };
+	data = $(this).serialize() + "&" + $.param(data);
+	$.ajax( {
+		method: "POST",
+		url: model.scriptURL,
+		dataType:"json",
+		data:data,
+		success: function (data){
+			model.updateModel(data["json"]);
+		}
+	});
+}
+
+model.resetData = function(){
+	var data = {
+	      "action": "reset"
+	  };
+	data = $(this).serialize() + "&" + $.param(data);
+	$.ajax( {
+		method: "POST",
+		url: model.scriptURL,
+		dataType:"json",
+		data:data,
+		success: function (data){
+			model.updateModel(data["json"]);
+		}
+	});
+}
+//console.log(model);
+
+$( document ).ready(function (){
+	// setup button handlers
+	
+	$("#loadButton").click(function (){
+		// call the php script
+		model.getData();
+	});
+
+	$("#saveButton").click(function (){
+		// call the php script
+		model.saveData();
+	});
+
+	$("#resetButton").click(function () {
+		// call the php script
+		model.resetData();
+	});
+
+	model.getData();
+
+});
